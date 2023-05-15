4d511bb1160a1703d00d22b0c1942851a159a776
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-05-15 22:40:59 +0300

    - Lint files

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 class Person
   def initialize(
-    age, 
+    age,
     parent_permission = true,
-    name = "Unknown"
+    name = 'Unknown'
   )
     @age = age
     @id = Random.rand(1...1000)
@@ -10,18 +10,16 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name, :age
   attr_reader :id
 
   def can_use_services?
-    self.of_age?
+    of_age?
   end
 
   private
-  
+
   def of_age?
     @age > 18
   end
-
 end
